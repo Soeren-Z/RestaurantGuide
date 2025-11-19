02b4eaa13021cc2124ba0dd234020792e367f846
Harrison Pape <harrison.pape@georgebrown.ca>
2025-11-19 14:08:15 -0500

    Add basic splash screen

---
+++ app/src/main/java/com/example/restaurantguide/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.restaurantguide;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
