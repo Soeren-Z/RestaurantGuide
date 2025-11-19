db5e8ff696b7b14a62ab570ff54e97c1260be19d
Harrison Pape <harrison.pape@georgebrown.ca>
2025-11-19 15:49:07 -0500

    ID fixes

--- app/src/main/java/com/example/restaurantguide/TeamInfoActivity.java
+++ app/src/main/java/com/example/restaurantguide/TeamInfoActivity.java
@@ -14,7 +14,7 @@ public class TeamInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_team_info_activity);
+        setContentView(R.layout.activity_team_info);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
