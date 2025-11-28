b3c0376470ba64d14b02208440696927be03e003
Areeb Shoukat <areeb.shoukat@georgebrown.ca>
2025-11-27 19:22:09 -0500

    Added Restaurant Details screen functionality (map, share, image placeholder, edit stub)

---
+++ app/src/main/java/com/example/restaurantguide/EditRestaurantActivity.java
@@ -0,0 +1,14 @@
+package com.example.restaurantguide;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_restaurant);
+        // This simply reuses the Add/Edit screen layout
+    }
+}
